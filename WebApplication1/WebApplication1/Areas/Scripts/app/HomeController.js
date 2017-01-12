1963492a775e37b1877837eb44206627e316b939
Aditya Naagar <naagar@live.in>
2017-01-13 00:51:25 +0530

    poc for multi lang support

---
+++ WebApplication1/WebApplication1/Areas/Scripts/app/HomeController.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module("app", ["ngResource"]);
+app.controller("MyCtrl", ["$scope", "translationService",
+    function ($scope, translationService) {
+    $scope.hello = "Hello World !";
+    $scope.lang ="en";
+    $scope.translate = function () {
+        translationService.getTranslation($scope, $scope.lang);
+    };
+    $scope.translate();
+}]);
+
+app.service("translationService", function ($resource) {
+    this.getTranslation = function ($scope, lang) {
+        var translationFilePath = "/Areas/dictionary/translation_"+lang+".json";
+        $resource(translationFilePath).get(function(data){
+            $scope.translation = data;
+        });
+        
+    };
+});
